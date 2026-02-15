8e2e1a77e3e6a238f384a3a562ea7eb4f46d5297
Amir Samson <amirhsamson93@gmail.com>
2026-02-15 19:09:25 +0330

    Added the Testsuit.xml file for TestNG and also added new Test file in Test folder.

---
+++ appium-java/src/test/java/com/amir/appium/HomePageTest.java
@@ -0,0 +1,12 @@
+package com.amir.appium;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+    @Test
+    public void validateHomePageTest(){
+        System.out.println("HomePage test is running");
+    }
+    
+}
--- appium-java/src/test/java/com/amir/appium/SampleTest.java
+++ appium-java/src/test/java/com/amir/appium/SampleTest.java
@@ -8,4 +8,9 @@ public class SampleTest {
     public void sanityTest() {
         System.out.println("Test framework is working");
     }
+    @Test
+    public void sanityTestNumberTwo() {
+        System.out.println("Number Two Test framework is working");
+    }
 }
+
